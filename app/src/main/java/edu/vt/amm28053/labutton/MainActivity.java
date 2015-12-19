be4e10ecc2e81d73542468e3e84a2e2a1d7af1e1
Alex Marshall <Alex Marshall>
2015-12-19 10:59:56 -0500

    Now randomly choses an audio file to play. Audio file resource ids are hardcoded, but might change in the future

--- app/src/main/java/edu/vt/amm28053/labutton/MainActivity.java
+++ app/src/main/java/edu/vt/amm28053/labutton/MainActivity.java
@@ -7,7 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    /*
+     *  These are all the audio files that are contained in the app. If new audio files are added
+     *  in the future, they will also need to be added here in order to be played.
+     */
+    private static final int[] AUDIO_FILES = { R.raw.anyoneelse, R.raw.doit, R.raw.dreams,
+            R.raw.dreamscometrue, R.raw.givingup, R.raw.impossible, R.raw.success, R.raw.yesterday,
+            R.raw.yesyoucan };
 
     private Button labutton;
 
@@ -21,10 +31,18 @@ public class MainActivity extends AppCompatActivity {
         labutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MediaPlayer mPlayer = MediaPlayer.create(MainActivity.this, R.raw.doit);
+                MediaPlayer mPlayer = MediaPlayer.create(MainActivity.this, getRandomAudioFile());
                 mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
                 mPlayer.start();
             }
         });
+    }
+
+    /**
+     * Returns the resource ids of one of the audio files, chosen "Randomly"
+     * @return the resource id of the audio file chosen
+     */
+    private int getRandomAudioFile() {
+        return AUDIO_FILES[new Random().nextInt(AUDIO_FILES.length)];
     }
 }
