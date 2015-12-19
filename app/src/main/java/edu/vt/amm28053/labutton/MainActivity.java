9b20c0558cca52d915b70cd0b35f7c4df2c2b9ab
Alex Marshall <amhokies@gmail.com>
2015-12-19 17:21:57 -0500

    Changed app name to "Shia Button", new image, help text now added when the app is first started, but goes away once the button is pressed.

--- app/src/main/java/edu/vt/amm28053/labutton/MainActivity.java
+++ app/src/main/java/edu/vt/amm28053/labutton/MainActivity.java
@@ -4,8 +4,10 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -19,18 +21,19 @@ public class MainActivity extends AppCompatActivity {
             R.raw.dreamscometrue, R.raw.givingup, R.raw.impossible, R.raw.success, R.raw.yesterday,
             R.raw.yesyoucan };
 
-    private Button labutton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        labutton = (Button)findViewById(R.id.labutton);
+        ImageButton labutton = (ImageButton)findViewById(R.id.labutton);
+        final TextView helpText = (TextView)findViewById(R.id.help_text);
 
         labutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                helpText.setVisibility(View.GONE);
+
                 MediaPlayer mPlayer = MediaPlayer.create(MainActivity.this, getRandomAudioFile());
                 mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
                 mPlayer.start();
@@ -43,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
      * @return the resource id of the audio file chosen
      */
     private int getRandomAudioFile() {
-        return AUDIO_FILES[new Random().nextInt(AUDIO_FILES.length)];
+        int audioFileIndex = new Random().nextInt(AUDIO_FILES.length);
+
+        // Debugging output
+        Log.d("Labutton", "Randomly chosing file " + AUDIO_FILES[audioFileIndex] +
+                " at index " + audioFileIndex);
+
+        return AUDIO_FILES[audioFileIndex];
     }
 }
